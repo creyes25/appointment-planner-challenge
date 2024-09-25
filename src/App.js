6a364b19848dab6fb76e569108d60b9028898871
creyes25 <carolina.reyes2022@gmail.com>
2024-09-25 12:47:53 -0500

    fix code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -53,7 +53,7 @@ function App() {
         element={ <ContactsPage contactsList={contacts} addNewContact= {addNewContact} /> }/>
       <Route 
         path={ROUTES.APPOINTMENTS} 
-        element={ <AppointmentsPage appointments={appointments} contacts={contacts} newAppointment={newAppointment} />  }/>
+        element={ <AppointmentsPage appointmentList={appointments} contactsList={contacts} newAppointment={newAppointment} />  }/>
     </Route>
   ));
   
