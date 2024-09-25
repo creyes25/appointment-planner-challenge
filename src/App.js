267ef340597061124e96986a36246e1453c172fd
creyes25 <carolina.reyes2022@gmail.com>
2024-09-25 09:47:43 -0500

    setup functions and variable to keep track of contacts and appointment data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
+
+
 
 function App() {
   /*
@@ -10,16 +13,47 @@ function App() {
   contacts and appointments 
   */
 
+  const [contacts, setContacts] = useState([])
+  const [appointments, setAppointments] = useState([])
+
   /*
   Implement functions to add data to
   contacts and appointments
   */
 
+  const addNewContact = (name, phoneNum, email) => {
+    setContacts([
+      ...contacts, 
+      {
+      name: name,
+      phoneNumber: phoneNum,
+      email: email 
+      }
+    ])
+  }
+
+  const newAppointment = (name, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        name: name,
+        contact: contact,
+        date: date,
+        time: time
+      }
+    ])
+  }
+
+
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
       <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+      <Route 
+        path={ROUTES.CONTACTS} 
+        element={ <ContactsPage contactsList={contacts} addNewContact= {addNewContact} /> }/>
+      <Route 
+        path={ROUTES.APPOINTMENTS} 
+        element={ <AppointmentsPage appointments={appointments} contacts={contacts} newAppointment={newAppointment} />  }/>
     </Route>
   ));
   
