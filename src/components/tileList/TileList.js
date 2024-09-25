848a861a8f985c3ced9dde6234384ae4cc0b0ef6
creyes25 <carolina.reyes2022@gmail.com>
2024-09-25 10:32:31 -0500

    render a list of Tile components using the list of contacts

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({contactsList}) => {
   return (
     <div>
-      
+      {contactsList.map((contact, idx) => {
+        const {name , ...rest} = contact
+        return (
+          <Tile 
+            key={idx}
+            name={name}
+            description={rest} // contains all other properties
+        />
+        )
+      })}
     </div>
   );
 };
