f019c5371af6c0e56074ed883ab4faee61eb6e8d
creyes25 <carolina.reyes2022@gmail.com>
2024-09-25 15:36:54 -0500

    render a dropdown list of all contact list

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({
+  contacts,
+  onChange,
+  value,
+  name
+}) => {
   return (
-    <></>
+    <>
+      <select name={name} onChange={onChange} value={value} >
+        <option value='' defaultChecked>No Contact Selected</option>
+        {contacts.map((contact, idx) => {
+          return (
+            <option key={idx} value={contact.name}>{contact.name}</option>
+          )
+        })}
+      </select>
+    </>
   );
 };
